6a9b958ed698bdf4d92cce58f1512e8b8aad2879
Kitsune <87698261+Kat3110@users.noreply.github.com>
2023-07-27 18:51:26 +0300

    add style nav-menu

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,6 @@ export default function RootLayout({children}) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleMenuToggle = (isOpen) => {
-    // console.log('handleMenuToggle', isOpen)
     setIsMenuOpen(!isOpen);
   };
 
@@ -22,7 +21,6 @@ export default function RootLayout({children}) {
       <link rel='icon' href='orangeLink.svg'/>
       <meta name="description" content='Healthy food restaurant' />
       <body>
-        <div>{isMenuOpen ? 'true' : 'false'}</div>
         <NavMenu isOpen={isMenuOpen} onMenuToggle={handleMenuToggle} />
         <Header isOpen={isMenuOpen} onMenuToggle={handleMenuToggle} />
         {children}
--- src/components/nav-menu/Nav-menu.jsx
+++ src/components/nav-menu/Nav-menu.jsx
@@ -1,8 +1,14 @@
 'use client'
-import React, { useState } from 'react';
+import React, {useState} from 'react';
+import Link from 'next/link'
 import './nav-menu.scss';
+import Image from 'next/image';
+import Instagram from '../../../public/Icon_instagram-min.svg';
+import Twitter from '../../../public/Icon_twitter-min.svg';
+import Facebook from '../../../public/Icon_facebook-min.svg';
+import Youtube from '../../../public/Icon_youtube-min.svg';
 
-export function NavMenu({ isOpen, onMenuToggle }) {
+export function NavMenu({isOpen, onMenuToggle}) {
   const [isMenuOpen, setIsMenuOpen] = useState(isOpen);
 
   const handleMenuClose = () => {
@@ -11,11 +17,88 @@ export function NavMenu({ isOpen, onMenuToggle }) {
   };
   return (
     <section className={`nav-menu ${isOpen ? 'nav-menu-open' : ''}`}>
-      <div>{isOpen ? 'true' : 'false'}</div>
-      <div className='nav-menu__close' onClick={handleMenuClose}>
+      <div className="nav-menu__close" onClick={handleMenuClose}>
         <div className="nav-menu__line nav-menu__line-1"></div>
         <div className="nav-menu__line nav-menu__line-2"></div>
       </div>
+      <div className="nav-menu__box">
+        <div className="nav-menu__links">
+          <Link
+            className="nav-item"
+            href={{
+              pathname: '/home',
+              name: 'Home'
+            }}
+          >
+            Home
+          </Link>
+          <Link
+            className="nav-item"
+            href={{
+              pathname: '/Menu',
+              name: 'Menu',
+            }}
+          >
+            Menu
+          </Link>
+          <Link
+            className="nav-item"
+            href={{
+              pathname: '/blogs',
+              name: 'Blogs',
+            }}
+          >
+            Blogs
+          </Link>
+          <Link
+            className="nav-item"
+            href={{
+              pathname: '/about',
+              name: 'About',
+            }}
+          >
+            About
+          </Link>
+          <Link
+            className="nav-item"
+            href={{
+              pathname: '/contact',
+              name: 'Contact',
+            }}
+          >
+            Contact
+          </Link>
+        </div>
+        <div className='nav-menu__contact'>
+          <h2 className='heading-five nav-menu__title'>Contact</h2>
+          <div className="divider"></div>
+          <div className='nav-menu__date' >
+            <div className="widgets-text nav-menu__connection">
+              <a href="tel:+1+86852346000">+1+86 852 346 000</a>
+              <a href="mailto:info@foodzero.com">info@foodzero.com</a>
+            </div>
+            <a className="widgets-text nav-menu__map"
+               href="https://www.waze.com/en/live-map/directions/us/ca/los-angeles/1959-s-sepulveda-blvd?place=EjExOTU5IFMgU2VwdWx2ZWRhIEJsdmQsIExvcyBBbmdlbGVzLCBDQSA5MDAyNSwgVVNBIjESLwoUChIJZ6ni5nW7woARuwuOYCmN0j0Qpw8qFAoSCRvd0P9cusKAETmBZ6QcUSze">
+              1959 Sepulveda Blvd.<br /> Culver City, CA, 90230
+            </a>
+            <div className="nav-menu__social">
+              <a className="nav-menu__link" href="https://www.instagram.com/">
+                <Image src={Instagram} alt="Instagram"></Image>
+              </a>
+              <a className="nav-menu__link" href="https://twitter.com/">
+                <Image src={Twitter} alt="Twitter"></Image>
+              </a>
+              <a className="nav-menu__link" href="https://facebook.com/">
+                <Image src={Facebook} alt="Facebook"></Image>
+              </a>
+              <a className="nav-menu__link" href="https://www.youtube.com/">
+                <Image src={Youtube} alt="Youtube"></Image>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </section>
   )
 }
